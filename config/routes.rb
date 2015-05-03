3bd0196b764d0e3f1a5054843de8068d56e0d07e
Danny <Danny@MININT-9O9GQUS.(none)>
2015-05-02 22:48:57 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'leonardo#index'
 
+   get '/twitter-sentiment', to: 'twitter#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
